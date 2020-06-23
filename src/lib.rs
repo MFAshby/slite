c4cddf757ae70b7e8d8e4e7682619988408df96f
Martin Ashby <martin@ashbysoft.com>
2020-06-23 22:45:08 +0100

    more WIP. A UI now appears, and channels are saved to a database table

--- src/lib.rs
+++ src/lib.rs
@@ -1,132 +1,120 @@
 pub mod cfg {
     use std::path::Path;
+    use config::{Config, Environment, File};
 
-    pub trait Cfg {
-        fn dbtype(&self) -> &str;
-        fn dbfile(&self) -> &Path;
+    pub struct Cfg {
+        config: Config,
     }
 
-    pub fn load() -> Box<Cfg> {
-        use config_rs::load;
-        load()
+    impl Cfg {
+        pub fn dbfile(&self) -> &Path {
+            Path::new(self.config.get("dbfile").unwrap_or("slite.db"))
+        }
+        pub fn api_key(&self) -> String {
+            self.config.get("api_key").expect("api_key must be set in config!")
+        }
     }
 
-    mod config_rs {
-        use super::*;
-        use config::{Config, Environment, File};
-
-        struct CfgRs {
-            config: Config,
-        }
-
-        impl Cfg for CfgRs {
-            fn dbtype(&self) -> &str {
-                self.config.get("dbtype").unwrap_or("sqlite")
-            }
-
-            fn dbfile(&self) -> &Path {
-                Path::new(self.config.get("dbfile").unwrap_or("slite.db"))
-            }
-        }
-
-        pub(super) fn load() -> Box<dyn Cfg> {
-            let mut c = Config::new();
-            Box::new(CfgRs {
-                config: c
-                    .merge(File::with_name("slite"))
-                    .unwrap()
-                    .merge(Environment::with_prefix("SLITE"))
-                    .unwrap()
-                    .to_owned(),
-            })
+    pub fn load() -> Cfg {
+        let mut c = Config::new();
+        Cfg {
+            config: c
+                .merge(File::with_name("slite"))
+                .unwrap()
+                .merge(Environment::with_prefix("SLITE"))
+                .unwrap()
+                .to_owned(),
         }
     }
 }
 
 pub mod db {
     use crate::cfg::Cfg;
-    use std::path::Path;
+    use rusqlite::{Connection,params};
+    use std::error::Error;
 
-    pub trait Db {
-        // Add database methods here.
-        // Take care not to expose sqlite details, just work on domain models (i.e. what slack
-        // crate gives us)
+    pub struct Db {
+        conn: Connection,
     }
 
-    pub fn new(c: &dyn Cfg) -> Box<dyn Db> {
-        let dbtype = c.dbtype();
-        let db = match dbtype {
-            "sqlite" => sqlite::new(c.dbfile()),
-            _ => panic!("Unexpected database type requested {}", dbtype),
-        };
-        Box::new(db)
+    impl Db {
+        pub fn add_channel(&self, id: &str, name: &str) -> Result<(), Box<dyn Error>> {
+            self.conn.execute("
+                insert into channel (id, name) values (?1, ?2)
+                ", params![id, name],).map_err(Box::new)?;
+            Ok(())
+        }
+
+        pub fn delete_channels(&self) -> Result<(), Box<dyn Error>> {
+            self.conn.execute("
+                delete from channel
+                ", params![],).map_err(Box::new)?;
+            Ok(())
+        }
     }
 
-    mod sqlite {
-        use super::*;
-        use rusqlite::Connection;
-
-        pub(super) struct SqliteDb {
-            conn: Connection,
-        }
-
-        impl Db for SqliteDb {}
-
-        pub(super) fn new(file: &Path) -> SqliteDb {
-            let connection = Connection::open(file)
-                .expect(format!("Unable to open database {:?}", file).as_str());
-            // TODO: MFA - Add schema migrations
-            SqliteDb { conn: connection }
-        }
+    pub fn load(c: &Cfg) -> Result<Db, Box<dyn Error>> {
+        let file = c.dbfile();
+        let conn = Connection::open(file).map_err(Box::new)?;
+        conn.execute("
+            create table if not exists channel (
+                id text primary key,
+                name text 
+                );
+            ", params![],).map_err(Box::new)?;
+        Ok(Db { conn })
     }
 }
 
-mod rtm_listener {
+pub mod rtm_listener {
     use std::error::Error;
-
-    use slack;
     use slack::{Event, RtmClient};
+    use crate::db::{Db};
+    use crate::cfg::{Cfg};
 
-    struct Handler;
+    struct Handler<'a> {
+        d: &'a Db
+    }
 
     #[allow(unused_variables)]
-    impl slack::EventHandler for Handler {
+    impl slack::EventHandler for Handler<'_> {
         fn on_event(&mut self, cli: &RtmClient, event: Event) {
             // Update the database with the new information received
-            println!("on_event(event: {:?})", event);
+            // println!("on_event(event: {:?})", event);
         }
 
         fn on_close(&mut self, cli: &RtmClient) {
             // Oops, uh, reconnect I guess
-            println!("on_close");
+            // println!("on_close");
         }
 
         fn on_connect(&mut self, cli: &RtmClient) {
             // Update the database with start_response info
-            println!("on_connect");
+            // println!("on_connect");
 
             // Sample code
             // find the general channel id from the `StartResponse`
             let channels = cli.start_response().channels.as_ref();
+            self.d.delete_channels()
+                .expect("Failed to delete channels");
+            
             println!("channels: {:?}", channels);
-            let general_channel_id = channels
-                .and_then(|channels| {
-                    channels.iter().find(|chan| match chan.name {
-                        None => false,
-                        Some(ref name) => name == "ai",
-                    })
-                })
-                .and_then(|chan| chan.id.as_ref())
-                .expect("ai channel not found");
-            let _ = cli
-                .sender()
-                .send_message(&general_channel_id, "Hello world! (rtm)");
+            for chan in channels.unwrap().iter() {
+                self.d.add_channel(chan.id.as_ref().unwrap(), chan.name.as_ref().unwrap())
+                    .expect("Failed to insert channel");
+            }
+            //let _ = cli
+            //    .sender()
+            //    .send_message(&general_channel_id, "Hello world! (rtm)");
             // Send a message over the real time api websocket
         }
     }
 
-    pub fn start(api_key: &str) -> Result<(), Box<dyn Error>> {
-        let mut handler = Handler;
+    pub fn start(c: &Cfg, d: &Db) -> Result<(), Box<dyn Error>> {
+        let api_key = c.api_key();
+        let mut handler = Handler {
+            d
+        };
         RtmClient::login_and_run(&api_key, &mut handler).map_err(Box::from)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,21 @@
-extern crate gio;
-extern crate gtk;
+use druid::{AppLauncher,WindowDesc,Widget};
+use druid::widget::Label;
 use slite::cfg;
+use slite::db;
+use slite::rtm_listener;
+use std::thread::spawn;
+use std::error::Error;
 
-use gio::prelude::*;
-use gtk::prelude::*;
-
-use gtk::{ApplicationWindow, Builder, Button, MessageDialog};
-
-use std::env::args;
-
-fn build_ui(application: &gtk::Application) {
-    let glade_src = include_str!("builder_basics.glade");
-    let builder = Builder::new_from_string(glade_src);
-
-    let window: ApplicationWindow = builder.get_object("window1").expect("Couldn't get window1");
-    window.set_application(Some(application));
-    let bigbutton: Button = builder.get_object("button1").expect("Couldn't get button1");
-    let dialog: MessageDialog = builder
-        .get_object("messagedialog1")
-        .expect("Couldn't get messagedialog1");
-
-    bigbutton.connect_clicked(move |_| {
-        dialog.run();
-        dialog.hide();
-    });
-
-    window.show_all();
+fn build_ui() -> impl Widget<()> {
+    Label::new("Hello World!")
 }
 
-fn main() {
-    let application = gtk::Application::new(
-        Some("com.github.gtk-rs.examples.builder_basics"),
-        Default::default(),
-    )
-    .expect("Initialization failed...");
-
-    application.connect_activate(|app| {
-        build_ui(app);
+fn main() -> Result<(), Box<dyn Error>> {
+    let c = cfg::load();
+    let d = db::load(&c)?;
+    spawn(move || {
+        let _ = rtm_listener::start(&c, &d);
     });
-
-    application.run(&args().collect::<Vec<_>>());
+    AppLauncher::with_window(WindowDesc::new(build_ui)).launch(()).map_err(Box::new)?;
+    Ok(())
 }
